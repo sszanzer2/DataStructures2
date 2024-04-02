72d41c37766b0aba921a198e7eebab662f50b768
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2024-04-02 14:28:58 -0400

    Update HashFunction.java

--- LambdasMaps/HashFunction.java
+++ LambdasMaps/HashFunction.java
@@ -2,6 +2,7 @@ package HashMap;
 
 /* 
  * Enum representing different types of hash functions.
+ * Not used in this version but was used in the last one
  */
 public enum HashFunction {
    NAIVE,          // Represents a naive hash function
